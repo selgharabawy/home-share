d7bed7c33edf6f23544169ce6cc87ba5a00db228
Sherif Gharabawy <selgharabawy@outlook.com>
2024-01-07 06:27:10 +0200

    implement jwt apis and setup

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,13 @@
 """
 Views for the API.
 """
-from rest_framework import generics
+from rest_framework import generics, status, permissions, authentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.token_blacklist.models import (
+    BlacklistedToken, OutstandingToken
+)
 
 from user.serializers import (
     UserSerializer,
@@ -11,3 +17,32 @@ from user.serializers import (
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            # blacklist all outstanding tokens for the user
+            for token in OutstandingToken.objects.filter(user=request.user):
+                BlacklistedToken.objects.get_or_create(token=token)
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticatd user."""
+        return self.request.user
