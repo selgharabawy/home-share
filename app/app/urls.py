98bcbdd2fdbe87a9198531f589915f419e480f85
Sherif Gharabawy <selgharabawy@outlook.com>
2024-01-09 21:03:38 +0200

    Add v1 to endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,13 +26,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
-        'api/docs/',
+        'api/v1/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
-    path('api/user/', include('user.urls')),
+    path('api/v1/user/', include('user.urls')),
 ]
 
 
